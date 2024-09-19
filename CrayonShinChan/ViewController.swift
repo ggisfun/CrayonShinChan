65493aa103b4ae527c8e70c282530511de461ac6
Adam <bgisgone@gmail.com>
2024-09-19 16:00:27 +0800

    Initial Commit

---
+++ CrayonShinChan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrayonShinChan
+//
+//  Created by Adam Chen on 2024/9/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
