d6d15709e8645a04ecead53dd6496120c39f2605
Adam <bgisgone@gmail.com>
2024-09-20 13:50:05 +0800

    蠟筆小新電影海報by CollectionView Custom Layout

---
+++ CrayonShinChan/CrayonCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CrayonCollectionViewCell.swift
+//  CrayonShinChan
+//
+//  Created by Adam Chen on 2024/9/19.
+//
+
+import UIKit
+
+class CrayonCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CrayonShinChan/CrayonCollectionViewLayout.swift
@@ -0,0 +1,12 @@
+//
+//  CrayonCollectionViewLayout.swift
+//  CrayonShinChan
+//
+//  Created by Adam Chen on 2024/9/19.
+//
+
+import UIKit
+
+class CrayonCollectionViewLayout: UICollectionViewLayout {
+
+}
